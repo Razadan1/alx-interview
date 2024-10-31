3f779a550294ea05bcf2163b1bdfa784cb803248
Razadan1 <danmoleomomayowa@gmail.com>
2024-10-31 14:21:32 +0100

    Task 0

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" UTF-8 validation """
+
+
+def validUTF8(data):
+    """ main function """
+    flag = False
+    jump_list = {30: 3, 14: 4, 6: 5}
+    char_length = {3: 3, 4: 2, 5: 1}
+    list_length = 0
+    if (len(data) == 0) or (len(data) == 1 and data[0] >> 7 == 0):
+        return True
+    
