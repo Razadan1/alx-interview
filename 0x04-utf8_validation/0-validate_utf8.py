6916835a6bd339b4c895a8a570b0293c2041651e
Razadan1 <danmoleomomayowa@gmail.com>
2024-10-31 14:23:10 +0100

    Updated Commit

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -17,4 +17,21 @@ def validUTF8(data):
             flag = False
         if len(bin(num)[2:]) == 9:
             num = int(bin(num)[3:], 2)
-        
+        if num >> 7 != 0 and len(bin(num)[2:]) >= 8:
+            if (not flag and num >> 6 == 2):
+                return False
+            elif (flag and num >> 6 != 2):
+                return False
+            for test_point, shift in jump_list.items():
+                if num >> shift == test_point:
+                    list_length = char_length[shift]
+                    break
+            if not (list_length or flag):
+                return False
+            else:
+                flag = True
+        elif num >> 7 == 0 and flag:
+            return False
+    if list_length:
+        return False
+    return True
